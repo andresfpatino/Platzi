84ad27db01ac0764995d47a607414e50d3964f68
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-11-17 21:44:15 -0500

    Formato de texto, alterando, quitando espacios, reemplazando, limpando

--- curso manejo de datos en php/3-formato-de-datos.php
+++ curso manejo de datos en php/3-formato-de-datos.php
@@ -9,3 +9,15 @@ echo lcfirst($text) . '<br>';     // primera letra minuscula
 
 
 // Reemplazar strings
+$slug = str_replace(' ', '-', $text); // reemplaza espacios por guiones
+echo strtolower($slug) . '<br>';
+
+// Modificación de texto
+$code = 39;
+echo str_pad($code, 8, '#', STR_PAD_LEFT) . '<br>'; // rellenar con # a la izquierda
+
+echo strip_tags($text) . '<br>'; // elimina todas las etiquetas html
+
+$fecha = "Año 2021, día de la programación";
+echo strtoupper($fecha) . '<br>'; //monobyte
+echo mb_strtoupper($fecha); //multibyte
