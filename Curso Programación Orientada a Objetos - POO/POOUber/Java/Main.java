9a468cc8de995048ef6273116329a76378cc1ad2
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-11-30 21:39:56 -0500

    Objetos en Java

--- Curso Programación Orientada a Objetos - POO/POOUber/Java/Main.java
+++ Curso Programación Orientada a Objetos - POO/POOUber/Java/Main.java
@@ -1,8 +1,17 @@
-
-package Java;
-
 class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+    public static void main(String[] args) {  
+
+        Car car = new Car();
+        car.license = "MWW055";
+        car.drive = "Andrés Patiño";
+        car.passenger = 4;
+
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "SGE37E";
+        car2.drive = "Fulanito";
+
+        car2.printDataCar();
     }
 }
