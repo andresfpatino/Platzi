27ae2d3cbb9bdcb6deb3719dee21c119136eecdc
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-12-03 23:20:16 -0500

    Objecto con un constructor JS

--- Curso Programación Orientada a Objetos - POO/POOUber/JS/Car.js
+++ Curso Programación Orientada a Objetos - POO/POOUber/JS/Car.js
@@ -1,6 +1,10 @@
-function Car(){
+function Car(license, driver){
     this.id;
-    this.license;
-    this.driver;
+    this.license = license;
+    this.driver = driver;
     this.passenger;
+}
+
+Car.prototype.printDataCar = function(){
+    console.log("License: " + this.license + " - Driver: " + this.driver.name);
 }
