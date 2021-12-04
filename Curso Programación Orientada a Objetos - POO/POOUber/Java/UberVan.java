9e8574e5e83a1f4b1b6112e53df5a7431beb70b3
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-12-03 23:36:39 -0500

    Aplicando herencia en Java

---
+++ Curso Programación Orientada a Objetos - POO/POOUber/Java/UberVan.java
@@ -0,0 +1,15 @@
+class UberVan extends Car {
+
+    Map<String, ArrayList<String, Integer>> typeCarAccepted;
+    ArrayList<String> seatsMaterial;
+
+    public UberVan(
+        String Licence, 
+        Account driver,
+        Map<String, ArrayList<String, Integer>> typeCarAccepted,
+        ArrayList<String> seatsMaterial){
+            super(license, driver);
+            this.typeCarAccepted = typeCarAccepted;
+            this.seatsMaterial = seatsMaterial;
+    }
+}
