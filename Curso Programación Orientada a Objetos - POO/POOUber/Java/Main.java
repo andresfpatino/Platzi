3b7c2bfe341c145d09bf685203d41657ed892c14
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-12-08 16:04:37 -0500

    Encapsulando atributos en Java

--- Curso Programación Orientada a Objetos - POO/POOUber/Java/Main.java
+++ Curso Programación Orientada a Objetos - POO/POOUber/Java/Main.java
@@ -2,10 +2,11 @@ class Main {
     public static void main(String[] args) {  
 
         UberX UberX = new UberX("MWW055", new Account("Andrés Patiño", "1144185142"), "Chevrolet", "Sonic");
-        UberX.passenger = 4;
+        UberX.setPassenger(4);
         UberX.printDataCar();
 
-        Car car2 = new Car("SGE37E", new Account("Fulanito", "1144236898"));
-        car2.printDataCar();
+        // Car car2 = new Car("SGE37E", new Account("Fulanito", "1144236898"));
+        // car2.printDataCar();
     }
 }
+
