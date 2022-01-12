672056026e3a1fade831ba00ec0822b56169c484
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-01-12 16:43:35 -0500

    Llamado al API

--- Curso practico de WordPress/platzigifts/functions.php
+++ Curso practico de WordPress/platzigifts/functions.php
@@ -24,7 +24,14 @@ function template_styles(){
 
     wp_enqueue_script( 'custom', get_template_directory_uri()."/assets/js/custom.js", false,"1.1", true );
 
-    wp_localize_script('custom', 'pg', array('ajaxurl' => admin_url('admin-ajax.php')));
+    wp_localize_script(
+        'custom',
+        'pg',
+        array(
+            'ajaxurl' => admin_url('admin-ajax.php'),
+            'apiurl' => home_url('/wp-json/pg/v1/')
+        )
+    );
 }
 
 add_action('wp_enqueue_scripts','template_styles');
@@ -149,8 +156,8 @@ add_action('rest_api_init', function (){
         )
     );
 });
-
-function novedadesAPI($data){
+function novedadesAPI($data)
+{
     $args = array(
         'post_type' => 'post',
         'posts_per_page' => $data['cantidad'],
