ef2f1cb68797577d55e7db30f35fcc087578f618
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-01-19 17:54:07 -0500

    Login form

--- Curso de Creación de Plugins para WordPress/wp-content/plugins/frontend-login/plugin-frontend-login.php
+++ Curso de Creación de Plugins para WordPress/wp-content/plugins/frontend-login/plugin-frontend-login.php
@@ -14,4 +14,6 @@
  */
 
 
- require_once(plugin_dir_path(__FILE__) . 'public/shortcode/form-registro.php');
+require_once(plugin_dir_path(__FILE__) . 'public/shortcode/form-registro.php');
+
+require_once(plugin_dir_path(__FILE__) . 'public/shortcode/form-login.php');
