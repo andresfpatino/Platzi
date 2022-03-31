4e83bcab1065949013998cb8980a122d0c548f4d
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-03-31 18:18:37 -0500

    Object.values() ES8+

--- Curso ECMAScript 6+/src/es8/index.js
+++ Curso ECMAScript 6+/src/es8/index.js
@@ -8,3 +8,14 @@ const team = {
 const entries = Object.entries(team);
 console.log(entries);
 console.log(entries.length);
+
+// 2. Object.values()
+const team2 = {
+    frontend: 'Jorge',
+    backend: 'Juan',
+    design: 'Pedro'
+}
+
+const values = Object.values(team2);
+console.log(values);
+console.log(values.length);
