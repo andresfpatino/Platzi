a52af62d09eca584887fffee7d52fb4bd1384508
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-03-31 18:16:32 -0500

    Object.entries() ES8+

---
+++ Curso ECMAScript 6+/src/es8/index.js
@@ -0,0 +1,10 @@
+// 1. Object.entries()
+const team = {
+    frontend: 'Jorge',
+    backend: 'Juan',
+    design: 'Pedro'
+}
+
+const entries = Object.entries(team);
+console.log(entries);
+console.log(entries.length);
