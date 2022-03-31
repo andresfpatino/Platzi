8da300efa6f2f4b2d6e7b2cd3573199f44bf2f00
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-03-31 17:59:09 -0500

    Includes ES7+

---
+++ Curso ECMAScript 6+/src/es7/index.js
@@ -0,0 +1,8 @@
+// 1. Includes ES7+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
+if(numbers.includes(7)){
+    console.log('The array includes 7');
+} else {
+    console.log('The array does not include 7');
+}
