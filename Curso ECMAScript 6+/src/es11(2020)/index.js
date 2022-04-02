8fb1789ad85d1a0fd182847a5fbb9f2fe20caac3
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:22:08 -0500

    BigInt methods

--- Curso ECMAScript 6+/src/es11(2020)/index.js
+++ Curso ECMAScript 6+/src/es11(2020)/index.js
@@ -5,3 +5,9 @@ button.addEventListener('click', async function(){
     const module = await import('./file.js');
     module.hello();
 })
+
+// 2. BigInt methods
+const bigNumber = 9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+console.log(bigNumber);
+console.log(anotherBigNumber);
