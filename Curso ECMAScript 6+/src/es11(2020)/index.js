d308a116e1d950ae288c0dd475f6ef6fe9674a63
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:26:04 -0500

    Promise.allSettled()

--- Curso ECMAScript 6+/src/es11(2020)/index.js
+++ Curso ECMAScript 6+/src/es11(2020)/index.js
@@ -11,3 +11,11 @@ const bigNumber = 9007199254740991n;
 const anotherBigNumber = BigInt(9007199254740991);
 console.log(bigNumber);
 console.log(anotherBigNumber);
+
+// 3. Promise.allSettled()
+const promise1 = new Promise((resolve,reject) => reject("reject"));
+const promise2 = new Promise((resolve,reject) => resolve("resolve"));
+const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
