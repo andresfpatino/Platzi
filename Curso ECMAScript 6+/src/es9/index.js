d1ac24c9ed5a729fd3057aef9840f15a20bfc283
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 15:55:42 -0500

    Regex

--- Curso ECMAScript 6+/src/es9/index.js
+++ Curso ECMAScript 6+/src/es9/index.js
@@ -35,3 +35,12 @@ helloWorld()
   .then(response => console.log(response))
   .catch(error => console.log(error))
   .finally(() => console.log('finalizó'))
+
+
+// 4. Regex
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2022-04-02');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+console.log(year, month, day);
