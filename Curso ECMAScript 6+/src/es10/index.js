1d927f2de4af75fb94153c5d04bbf3e880170f10
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:06:30 -0500

    Optional catch binding

--- Curso ECMAScript 6+/src/es10/index.js
+++ Curso ECMAScript 6+/src/es10/index.js
@@ -15,3 +15,10 @@ console.log(hello.trimStart());
 let hello2 = 'hello world                  ';
 console.log(hello2);
 console.log(hello2.trimEnd());
+
+// 5. Optional catch binding
+try {
+
+} catch {
+    error
+}
