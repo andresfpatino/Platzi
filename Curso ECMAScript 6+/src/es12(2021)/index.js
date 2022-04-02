a0d88bfbaf6be513296bdc0b301df435fc1976ad
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:37:09 -0500

    ReplaceAll ES12

---
+++ Curso ECMAScript 6+/src/es12(2021)/index.js
@@ -0,0 +1,4 @@
+// 1. ReplaceAll
+const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
+const replacedString = string.replaceAll("JavaScript", "WordPress");
+console.log(replacedString);
