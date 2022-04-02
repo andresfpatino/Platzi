3bcb7cbe31abaa0fc034c755fd83d9567e5c0ed2
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:02:55 -0500

    Method flatMap

--- Curso ECMAScript 6+/src/es10/index.js
+++ Curso ECMAScript 6+/src/es10/index.js
@@ -1,3 +1,7 @@
 // 1. Method flat 
 let array = [1,2,3, [1,2,3, [1,2,3]]];
 console.log(array.flat(2)) // recive as param the number of levels to flatten
+
+// 2. Method flatMap
+let array2 = [1,2,3,4,5];
+console.log(array2.flatMap(value => [value, value * 2]));
