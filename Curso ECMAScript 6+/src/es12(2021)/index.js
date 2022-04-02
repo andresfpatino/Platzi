0d70a54c6c6b2912b44ce1308275558bda450d0c
Andrés Patiño <andresfelipepatino5@gmail.com>
2022-04-02 16:39:26 -0500

    Private methods #

--- Curso ECMAScript 6+/src/es12(2021)/index.js
+++ Curso ECMAScript 6+/src/es12(2021)/index.js
@@ -2,3 +2,13 @@
 const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
 const replacedString = string.replaceAll("JavaScript", "WordPress");
 console.log(replacedString);
+
+// 2. Private methods
+class Message {
+    #show(val){ // Con el # convertimos al método en privado
+        console.log(val);
+    };
+};
+
+const message = new Message();
+message.show('Hola!');
