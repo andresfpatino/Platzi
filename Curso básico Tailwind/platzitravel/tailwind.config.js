4f9ae6c26d70a0d5808c54c70cad193f3a4498f6
Andrés Patiño <andres@frontporchsolutions.com>
2022-04-16 16:18:43 -0500

    Source layout

--- Curso básico Tailwind/platzitravel/tailwind.config.js
+++ Curso básico Tailwind/platzitravel/tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
-  content: ['./public/index.html', './src/**/*.{html,js}'],
+  purge: [],
+  darkmode: false, // or 'media' or 'class'
   theme: {
+      extend: {
+          backgroundImage: {
+              'bali' : "url('../img/')",
+              'chicago' : "url('../img/')",
+              'europe' : "url('../img/')",
+              'iceland' : "url('../img/')",
+              'la' : "url('../img/')",
+              'miami' : "url('../img/')",
+              'newYork' : "url('../img/')",
+              'norWay' : "url('../img/')",
+              'sanFrancisco' : "url('../img/')",
+              'sanFrancisco' : "url('../img/')",
+              'sanFranciscoDesktop' : "url('../img/')",
+              'seattle' : "url('../img/')",
+              'switzerland' : "url('../img/')",
+              'sydney' : "url('../img/')",
+              'yosemite' : "url('../img/')"
+          },
+          backgroundColor: theme => ({
+              ...theme('colors'),
+              'primary': '#CC2D4A',
+          })
+      },
+  },
+  variants: {
       extend: {},
   },
+  content: ['./index.html', './src/**/*.{html,js}'],
   plugins: [],
 };
